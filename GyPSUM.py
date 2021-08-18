79aeae8ca8c6d259451ba7a2539b17e3134734a6
Tim-R413 <51277596+Tim-R413@users.noreply.github.com>
2021-08-18 17:31:22 -0400

    Update GyPSUM.py

--- GyPSUM.py
+++ GyPSUM.py
@@ -9,6 +9,7 @@ import time
 
 from feature_extraction import HyperCube
 from cluster import Cluster
+import numpy as np
 
 
 def main(argv):
@@ -18,7 +19,7 @@ def main(argv):
     start = time.time(), 
     cube = HyperCube(img_path= '/content/GyPSUM/ENVI_images/P306_1_1B4C1T4V.dat', hdr_path= '/content/GyPSUM/ENVI_images/P306_1_1B4C1T4V.hdr',band_wv='/content/GyPSUM/Band_wavelengths.npy')
     
-  
+    print('check first pixel',np.sum(cube.cube[0,0]))
     cube.unmask_value()
     
     cube.clip()
@@ -29,17 +30,26 @@ def main(argv):
     #cube.display_img()
     # cube.remove_continuum()
     cube.standardize()
-    #cube.set_n_components(20)
+    #cube.set_n_components(6)
 
+    print('check first pixel before feature extraction',np.sum(cube.cube[0,0]))
     cube.hysime()
     cube.autoencoder(epochs=3)
+    #print(cube.cube.shape)
     # cube.pca()
 
+    
+    number_clusters= cube.n_components
+    print('number of clusters to produce will be:',number_clusters )
+    
+    print('check first pixel before clustering',np.sum(cube.cube[0,0]))
     clus = Cluster(cube)
     # clus.k_means(5)
-    clus.gaussian_mixture(5)
+    clus.gaussian_mixture(6)#number_clusters)
     # clus.hierarchical_gaussian_mixture(4)
-    # clus.combine_spectrally_similar(15)
+    #clus.combine_spectrally_similar(6)
+    print(np.unique(clus.clus))
+    print(np.max(clus.clus))
 
     end = time.time()
    
